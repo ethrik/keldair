6b7506f6c7e0ac9b97d2bd1ede0927963af68ae2
Alexandria Marie Wolcott <alyx@woomoo.org>
2011-02-04 20:59:23 -0600

    First commit. Added VERY basic Keldair.pm

---
+++ lib/Keldair.pm
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+# Keldair.pm - IRC client library
+# Copyright 2011 Alexandria M. Wolcott <alyx@woomoo.org>
+# Licensed under the 3-clause BSD.
+
+use strict;
+use warnings;
+
+package Keldair;
+
+our (%V) = (
+    'MAJOR' => 0,
+    'MINOR' => 0,
+    'PATCH' => 0
+);
+
+our $VERSION = "$V{MAJOR}.$V{MINOR}.$V{PATCH}";
+
