316ef5dc32d01c85974649acfe24d5b9af2cdf66
Samuel Hoffman <minicruzer@live.com>
2011-02-05 12:46:36 -0500

    Add a perl "binary" to start our bot

---
+++ keldair.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+# COPYRIGHT!!! YOU MAY NOT USE IF YOU'RE FGT.
+# (fix this, alxy)
+use strict;
+use warnings;
+
+# Include the fucking lib/ folder with your silly little FindBin or whatever, alyx
+# Love, Sam.
