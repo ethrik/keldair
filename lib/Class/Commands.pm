e34cd77482a456b2c60ae8ba797c1f06fe8098cb
Samuel Hoffman <minicruzer@live.com>
2011-02-05 22:14:39 -0500

    Comments, Copyrights, and Unicorns

--- lib/Class/Commands.pm
+++ lib/Class/Commands.pm
@@ -1,17 +1,31 @@
+# Copyright 2011 Ethrik Project, et al.
+# Licensed under the 3-clause BSD
+# You are prohibited by law to run this file by perltidy or I will prosecute you -- Samuel Hoffman 2011
+
 package Class::Commands;
 use Mouse::Role;
 
+## joinChannel(str)
+# Attempt to join a channel
+# @chan Channel to join
 sub joinChannel {
 	my ($this, $chan) = @_;
 	$this->raw("JOIN $chan");
 }
 
+## raw(str)
+# Send a raw line to the server
+# @dat Data to send to the server
 sub raw {
     my ($this, $dat) = @_; 
     print $Class::Keldair::socket "$dat\n";
     print "S: $dat\n" if $this->debug;
 }
 
+## msg(str, str)
+# PRIVMSG a target (channel/user) with a message
+# @target Channel or User to message
+# @msg Text to send
 sub msg {
     my ($this, $target, $msg) = @_; 
     $this->raw("PRIVMSG $target :$msg");
