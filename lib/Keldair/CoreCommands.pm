712284ec087826db86a44f2ebd823ecc82ad4f17
Alyx <alyx@sporksmoo.net>
2011-03-15 00:30:43 -0500

    Moved trigger handling to CoreCommands, Help is now just help.

--- lib/Keldair/CoreCommands.pm
+++ lib/Keldair/CoreCommands.pm
@@ -6,31 +6,64 @@ use strict;
 use warnings;
 use Keldair;
 
-$keldair->command_bind(DIE => sub {
-	my ($chan, $dst, @reason) = @_;
+my $trigger;
 
-	$keldair->quit((join ' ', @reason));
-	$keldair->logf(INFO => 'Shutting down by request of %s from %s.', $dst->nick, $chan->name);
-	exit 0;
-});
+$keldair->hook_add(OnRehash => sub {
+        my $chan = shift;
+        $trigger = $keldair->config('keldair/trigger');
+        $keldair->msg($chan => 'Updating trigger to %s', $trigger) unless !$chan;
+        return 0;
+    }); 
+
+$keldair->hook_add(OnMessage => sub {
+        my ($chan, $nick, @msg) = @_;
+        
+        my $msg = join ' ', @msg;
+
+        my $cmd = substr $msg, length $trigger;
+        $cmd = (split ' ', $cmd)[0];
+        return 0 if !defined $cmd;
+
+        my $_trigger = substr $msg, 0, (length $trigger);
+
+        return 0 unless $trigger eq $_trigger;
+
+        my $trig_and_cmd = length($cmd) + length($trigger);
+        my $args = substr $msg, $trig_and_cmd;
+        my @args = split ' ', $args;
+
+        my $exec_cmd = $keldair->command_get(uc $cmd);
+        if($exec_cmd)
+        {
+            $exec_cmd->($keldair->find_chan($chan), $keldair->find_user($nick), @args);
+        }
+    });
+
+$keldair->command_bind(DIE => sub {
+        my ($chan, $dst, @reason) = @_;
+
+        $keldair->quit((join ' ', @reason));
+        $keldair->logf(INFO => 'Shutting down by request of %s from %s.', $dst->nick, $chan->name);
+        exit 0;
+    });
 
 $keldair->command_bind(RESTART => sub {
-	my ($chan, $dst, $reason) = @_;
+        my ($chan, $dst, $reason) = @_;
 
-	system 'perl keldair';
-	
-	$keldair->quit($reason);
-	$keldair->logf(INFO => 'Restarting by request of %s from %s.', $dst->nick, $chan->name);
-	
-	exit 0;
-});
+        system 'perl keldair';
+
+        $keldair->quit($reason);
+        $keldair->logf(INFO => 'Restarting by request of %s from %s.', $dst->nick, $chan->name);
+
+        exit 0;
+    });
 
 $keldair->command_bind(REHASH => sub {
-	my ($chan, $dst) = @_;
+        my ($chan, $dst) = @_;
 
-	$keldair->hook_run(OnRehash => $chan, $dst);
-	$keldair->msg($chan, 'Rehashing keldair.conf.');
-	$keldair->logf(INFO => '%s is rehashing keldair.conf.', $dst->nick);
-});
+        $keldair->hook_run(OnRehash => $chan, $dst);
+        $keldair->msg($chan, 'Rehashing keldair.conf.');
+        $keldair->logf(INFO => '%s is rehashing keldair.conf.', $dst->nick);
+    });
 
 1;
--- lib/Keldair/Module/Help.pm
+++ lib/Keldair/Module/Help.pm
@@ -7,39 +7,6 @@ use Keldair;
 use strict;
 use warnings;
 use Data::Dumper;
-my $trigger;
-
-$keldair->hook_add(OnRehash => sub {
-	my $chan = shift;
-	$trigger = $keldair->config('keldair/trigger');
-	$keldair->msg($chan => 'Updating trigger to %s', $trigger) unless !$chan;
-	return 0;
-});
-
-$keldair->hook_add(OnMessage => sub {
-	my ($chan, $nick, @msg) = @_;
-	print Dumper($chan)."\n\n";
-	print Dumper($nick)."\n\n";
-	my $msg = join ' ', @msg;
-
-	my $cmd = substr $msg, length $trigger;
-	$cmd = (split ' ', $cmd)[0];
-	return 0 if !defined $cmd;
-	
-	my $_trigger = substr $msg, 0, (length $trigger);
-
-	return 0 unless $trigger eq $_trigger;
-
-	my $trig_and_cmd = length($cmd) + length($trigger);
-	my $args = substr $msg, $trig_and_cmd;
-	my @args = split ' ', $args;
-
-	my $exec_cmd = $keldair->command_get(uc $cmd);
-	if($exec_cmd)
-	{
-		$exec_cmd->($keldair->find_chan($chan), $keldair->find_user($nick), @args);
-	}
-});
 
 $keldair->command_bind(HELP => sub {
 	my ($chan, $nick, $cmd) = @_;
