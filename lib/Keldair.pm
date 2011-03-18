973ce16212a040d307b726e59c94a5717aaecfc5
Alyx <alyx@sporksmoo.net>
2011-03-18 12:50:47 -0500

    Very basic hook constants for timers added. Not used atm, but will be once we implement nice timers.

--- lib/Keldair.pm
+++ lib/Keldair.pm
@@ -7,12 +7,12 @@ use warnings;
 use Class::Keldair;
 use Sys::Hostname;
 use base 'Exporter';
-our @EXPORT = qw($keldair &HOOK_DENY &HOOK_PASS &HOOK_DENY_EAT &HOOK_PASS_EAT);
+our @EXPORT = qw($keldair &HOOK_DENY &HOOK_PASS &HOOK_DENY_EAT &HOOK_PASS_EAT &TIMER_ONCE &TIMER_REPEAT);
 
 our (%V) = (
     'MAJOR' => 3,
     'MINOR' => 7,
-    'PATCH' => 1,
+    'PATCH' => 2,
 );
 
 our $VERSION = "$V{MAJOR}.$V{MINOR}.$V{PATCH}";
@@ -48,6 +48,11 @@ sub HOOK_PASS () { 1; } # Allow action to proceed.
 sub HOOK_DENY () { 2; } # Do not allow action to proceed.
 sub HOOK_DENY_EAT () { -2; } # Do not allow action to proceed, and eat event.
 sub HOOK_PASS_EAT () { -1; } # Allow action to proceed, and eat event.
+
+
+# Timer constant magics.
+sub TIMER_ONCE () { 1; } # Have the timer only run once.
+sub TIMER_REPEAT () { 2; } # Have the timer repeat continuously.
 
 # DENY = 1/-1
 # PASS = 2/-1
