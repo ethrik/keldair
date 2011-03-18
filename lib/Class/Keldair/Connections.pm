acf791b488412332fa6b011a07bf196a3559c0d2
Alyx <alyx@sporksmoo.net>
2011-03-17 23:06:16 -0500

    Get rid of obnoxious perlcritic issues.

--- lib/Class/Keldair/Connections.pm
+++ lib/Class/Keldair/Connections.pm
@@ -1,6 +1,5 @@
 # Copyright 2011 Ethrik Project, et al.
 # Licensed under the 3-clause BSD.
-# You are prohibited by law to run this file by perltidy or I will prosecute you -- Matthew Barskdale 2011
 package Class::Keldair::Connections;
 use Mouse;
 
--- lib/Class/Keldair/Parser.pm
+++ lib/Class/Keldair/Parser.pm
@@ -1,6 +1,5 @@
 # Copyright 2011 Ethrik Project, et al.
 # Licensed under the 3-clause BSD.
-# You are prohibited by law to run this file by perltidy or I will prosecute you -- Samuel Hoffman 2011
 package Class::Keldair::Parser;
 use Mouse::Role;
 use strict;
