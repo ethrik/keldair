965d0015d4289c14133557e4189be075174ac864
Matthew Barksdale <matthew@matt-tech.info>
2011-03-17 23:53:54 -0400

    Basic config lib including the alyx-perltidy-prevention clause

---
+++ lib/Keldair/Config.pm
@@ -0,0 +1,18 @@
+# Copyright 2011 Ethrik Project, et al.
+# Licensed under the 3-clause BSD.
+# You are prohibited by law to run this file by perltidy or I will prosecute you -- Matthew Barksdale 2011
+
+package Keldair::Config;
+use strict;
+use warnings;
+use Keldair;
+use Config::JSON;
+
+
+
+
+
+
+
+1;
+
