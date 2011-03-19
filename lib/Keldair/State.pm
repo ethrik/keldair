e915745a9395da6bd6391170b2fa71a92b76ac11
Samuel Hoffman <minicruzer@live.com>
2011-03-18 23:06:30 -0400

    Mildly fixed master to not be so broken. Fixed #4.

--- lib/Keldair/State.pm
+++ lib/Keldair/State.pm
@@ -25,7 +25,7 @@ $keldair->hook_add(OnJoin => sub {
 	$chan =~ s/:// if $chan =~ /^:(#|&|!).*/;
 	$keldair->raw("WHO $chan");
 	
-	my $_chan = Class::Keldair::Channel->new(name => $chan);
+	my $_chan = Class::Keldair::Channel->new(name => $chan, network => 'TODO');
 	
 	$keldair->add_chan($chan, $_chan);
 });
@@ -37,6 +37,7 @@ $keldair->hook_add(OnRaw352 => sub {
 			ident => $ident,
 			realname => $ident,
 			host => $host,
+			network => 'TODO',
 			server => $server
 		);
 		$keldair->log(STATE => "Adding $nick!$ident\@$host to $chan:Users - Adding $chan to $nick:Channels.");
