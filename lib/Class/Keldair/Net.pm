362bb4b4c32efd413f58e14874d58efa68d3e051
Samuel Hoffman <minicruzer@live.com>
2011-03-23 16:11:13 -0400

    Add a Net.pm to include with Class::Keldair, which will allow adding the socket object without having to pass it invidually

---
+++ lib/Class/Keldair/Net.pm
@@ -0,0 +1,80 @@
+# Copyright 2011 Ethrik Project, et al.
+# Licensed under the 3-clause BSD.
+package Class::Keldair::Net;
+use Mouse;
+
+has 'nick' => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1
+);
+
+has 'ident' => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1
+);
+
+has 'host' => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1
+);
+
+has 'realname' => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1
+);
+
+has 'server' => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1
+);
+
+has 'network' => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1
+);
+
+has 'channels' => (
+        traits => ['Hash'],
+        is => 'ro',
+        isa => 'HashRef[Object]',
+        default => sub { {} },
+        handles => {
+                add_chan => 'set',
+                is_in => 'get',
+                no_chans => 'is_empty',
+                del_chan => 'delete',
+                chan_pairs => 'kv'
+        }
+);
+
+has 'modes' => (
+	traits => ['Hash'],
+	is => 'ro',
+	isa => 'HashRef[Str]',
+	default => sub { {} },
+	handles => {
+		add_mode => 'set',
+		has_mode => 'get',
+		del_mode => 'delete',
+		no_modes => 'is_empty',
+		mode_pairs => 'kv'
+	}
+);
+
+has 'socket' => (
+	is => 'ro',
+	isa => 'Object',
+	default => sub {
+		my $this = shift; # The entire bot scope (config included)
+		# CREATE NORMAL SOCKET HEE!!!
+		IO::Socket::INET->new(...);
+	}
+);
+
+1;
