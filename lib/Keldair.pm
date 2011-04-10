814735d5f7e9a3e21e3296887c905c628909b195
Alyx <alyx@sporksmoo.net>
2011-04-09 20:27:05 -0500

    $database needs to go byebye for now.

--- lib/Keldair.pm
+++ lib/Keldair.pm
@@ -8,7 +8,7 @@ use FindBin qw($Bin);
 use Class::Keldair;
 use Sys::Hostname;
 use base 'Exporter';
-our @EXPORT = qw($keldair $database &HOOK_DENY &HOOK_PASS &HOOK_DENY_EAT &HOOK_PASS_EAT &TIMER_ONCE &TIMER_REPEAT);
+our @EXPORT = qw($keldair &HOOK_DENY &HOOK_PASS &HOOK_DENY_EAT &HOOK_PASS_EAT &TIMER_ONCE &TIMER_REPEAT);
 
 our (%V) = (
     'MAJOR' => 3,
@@ -19,13 +19,13 @@ our (%V) = (
 our $VERSION = "$V{MAJOR}.$V{MINOR}.$V{PATCH}";
 our $keldair = Class::Keldair->new();
 
-my $db = $keldair->conf->get('database');
-if (!$db || -w $db) {
-    $db = "$Bin/../etc/keldair.db";
-}
-Config::JSON->create($db) if (-w $db);
-our $database = Config::JSON->new($db)
-;
+#my $db = $keldair->conf->get('database');
+#if (!$db || -w $db) {
+#    $db = "$Bin/../etc/keldair.db";
+#}
+#Config::JSON->create($db) if (-w $db);
+#our $database = Config::JSON->new($db);
+
 $keldair->hook_add(OnPreConnect => sub {
 	my $network = shift;
 	$keldair->raw($network, "PASS ".$keldair->config("networks/$network/server/password")) if $keldair->config("networks/$network/server/password");
